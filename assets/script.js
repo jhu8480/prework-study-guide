096032a95213e81918bbc21ff863e6629854e8b1
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2022-11-23 14:10:55 -0500

    addedJavaScript

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topic = ['HTML','CSS','Git','JavaScript'];
+for(let i = 0; i < topic.length; i += 1) {
+  console.log(topic[i]);
+}
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
